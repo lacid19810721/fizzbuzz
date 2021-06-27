cfd0556ba3dc36e3746f7685e5a3a5a699489f83
lacid00 <lacid00@naver.com>
2021-06-27 18:40:52 +0900

    FizzBuzz Test case 추가 : I added 1~3 test for fizzbuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,9 +1,15 @@
 public class FizzBuzz {
     public String play(String s) {
+        int number = Integer.parseInt(s);
 
-        if("1".equals(s)) {
-            return "1";
+        if (number % 15 == 0) {
+            return "fizzbuzz";
+        } else if (number % 5 == 0) {
+            return "buzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
         }
-        return null;
+        return String.valueOf(number);
+
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -9,8 +9,23 @@ public class FizzBuzzTest {
         FizzBuzz fizzBuzz = new FizzBuzz();
         // act(코드실행)
         String result = fizzBuzz.play("1");
-
         assertEquals("1", result);
+    }
+    @Test
+    public void should_return_2_when_2_given() {
+        // arrange (데이터셋업)
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // act(코드실행)
+        String result = fizzBuzz.play("2");
+        assertEquals("2", result);
+    }
+    @Test
+    public void should_return_fizz_when_3_given() {
+        // arrange (데이터셋업)
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // act(코드실행)
+        String result = fizzBuzz.play("3");
+        assertEquals("fizz", result);
     }
 
 }
